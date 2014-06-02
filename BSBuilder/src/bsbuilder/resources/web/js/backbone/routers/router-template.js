b9f7a6439d7771b38bb1a11e75af0b31f625d78c
lrama1@gmail.com <lrama1@gmail.com@78aebb35-60c0-10aa-6c12-abaf023d7ced>
2014-06-02 02:21:05 +0000

    fixes for optimized builds

--- BSBuilder/src/bsbuilder/resources/web/js/backbone/routers/router-template.js
+++ BSBuilder/src/bsbuilder/resources/web/js/backbone/routers/router-template.js
@@ -23,6 +23,17 @@ define([
 		}		
 	});
 	
+	$(document).ready(function () {
+	    $('.nav li a').click(function(e) {
+	        $('.nav li').removeClass('active');
+	        var $parent = $(this).parent();
+	        if (!$parent.hasClass('active')) {
+	            $parent.addClass('active');
+	        }
+	        //e.preventDefault();
+	    });
+	});
+	
 	var initialize = function(){		
 		
      	// Initiate the router
