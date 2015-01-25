2a49ffa8fdde4e030c7f2081fa683b9c0532ac99
lrama1@gmail.com <lrama1@gmail.com@78aebb35-60c0-10aa-6c12-abaf023d7ced>
2015-01-25 22:02:02 +0000

    1.  added missing font 2.  re-engineer nav bar portion

--- BSBuilder/src/bsbuilder/wizards/site/NewBackboneSpringProjectWizard.java
+++ BSBuilder/src/bsbuilder/wizards/site/NewBackboneSpringProjectWizard.java
@@ -369,6 +369,9 @@ public class NewBackboneSpringProjectWizard extends Wizard implements
 			CommonUtils.addFileToProject(folders.get("src/main/webapp/WEB-INF/resources/js/fonts"), new Path("glyphicons-halflings-regular.woff"), 
 					this.getClass().getResourceAsStream("/bsbuilder/resources/fonts/glyphicons-halflings-regular.woff"), monitor);
 			
+			CommonUtils.addFileToProject(folders.get("src/main/webapp/WEB-INF/resources/js/fonts"), new Path("glyphicons-halflings-regular.woff2"), 
+					this.getClass().getResourceAsStream("/bsbuilder/resources/fonts/glyphicons-halflings-regular.woff2"), monitor);
+			
 			CommonUtils.addFileToProject(folders.get("src/main/webapp/WEB-INF/resources/js/libs"), new Path("backbone.global.js"), 
 					this.getClass().getResourceAsStream("/bsbuilder/resources/web/js/libs/backbone.global.js"), monitor);
 			
